a33737f29db0848638daa2df6adf4d71299784a0
Ramiro Ignacio Rios Lopez <rl.ramiro11@gmail.com>
2024-01-04 12:59:11 -0300

    init: release early version

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {Container, Image} from "@sveltestrap/sveltestrap";
+    import logo from '$lib/assets/favicon.png';
+</script>
+
+<style>
+    p {
+        text-align: center;
+    }
+    
+    .modal {
+        margin-top: 30px;
+    }
+</style>
+
+<Container class="modal" fluid>
+    <Image width="200" height="200" src={logo} alt="My logo"/>
+</Container>
