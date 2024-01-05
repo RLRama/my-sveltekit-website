0adbc4fe64e2d0def7620c77bdb0ee7ed4429a21
Ramiro Ignacio Rios Lopez <rl.ramiro11@gmail.com>
2024-01-05 12:57:13 -0300

    chore: wip development

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,36 @@
-<script>
-    import {Container, Image} from "@sveltestrap/sveltestrap";
+<svelte:head>
+    <title>Ramiro Ignacio Rios Lopez</title>
+    <meta name="robots" content="noindex nofollow"/>
+    <html lang="en" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</svelte:head>
+
+<script lang="ts">
     import logo from '$lib/assets/favicon.png';
+    import {Col, Container, Row} from "@sveltestrap/sveltestrap";
+
 </script>
 
+<img id="logo" alt="My logo" class="text-center d-block" src={logo} width="200"/>
+
+
+<Container id="global-container">
+
+
+    <Container id="heroSection">
+    </Container>
+
+    <Container id="projectsSection">
+
+    </Container>
+</Container>
+
 <style>
-    p {
-        text-align: center;
-    }
-    
-    .modal {
-        margin-top: 30px;
+
+    #logo {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
     }
 </style>
-
-<Container class="modal" fluid>
-    <Image width="200" height="200" src={logo} alt="My logo"/>
-</Container>
